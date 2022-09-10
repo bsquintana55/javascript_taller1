e2bc15785251d80da5c44ac1fed4ce19732ce77a
bsquintana55 <bsquintana55@misena.edu.co>
2022-09-10 14:50:21 -0500

    js

--- assets/js/taller.js
+++ assets/js/taller.js
@@ -25,14 +25,16 @@
     });
 
 
-    /*MINUSCULAS*/
+    //minuscula
     const minuscula = document.getElementById('minuscula');
     minuscula.addEventListener('click', ()=>{
         const palabra = document.getElementById('palabra').value;
         const result = palabra.toLowerCase();
         alert(`La palabra digitada ${palabra} a minúscula es ${result}`);
     });
-    /*PRIMERA LETRA */
+   
+
+    //crarcter 1
     const caracter = document.getElementById('caracter');
     caracter.addEventListener('click', ()=>{
 
@@ -42,5 +44,50 @@
         alert(`El primer caracter de la palabra digitada ${palabra} es ${result}`);
     });
 
+
+//2 temperature
+
+    
+        temperature.addEventListener('click', () =>{
+            //opcion de usuario
+            const opcion = document.getElementById('opcioon').value;
+            parseInt(opcion)
+            //input
+            const tempe = document.getElementById('tempe').value;
+            parseFloat(tempe)
+            let grado
+            parseFloat(grado)
+            //faren a cel
+            if (opcion==1) {
+                grado=((5(tempe-32))/9)
+
+            } 
+            // fran a kel
+            else if (opcion==2) {
+                grado=(((5(tempe-32))/9)+273.15)
+            } 
+            //cel a faren
+            else if (opcion==3){
+                grado=(((9*tempe)/5)+32)
+
+            } 
+            //cel a kel
+            else if (opcion==4){
+                grado=(grado+273.15)
+                
+            } 
+            //kel a faren
+            else if (opcion==5){
+                grado=(((9(tempe-273.15))/5)+32)
+
+            } 
+            //kel a cel
+            else if (opcion==6){
+                grado=(tempe-273.15)
+
+            }
+
+
+        })
 
 
