f4683702b303136ebce02f74061393f3fe7054cd
bsquintana55 <bsquintana55@misena.edu.co>
2022-09-10 15:49:34 -0500

    2 punto

--- assets/js/taller.js
+++ assets/js/taller.js
@@ -1,100 +1,103 @@
 
-    //puntos del taller
+        //puntos del taller
 
 
 
-    //1 de palabras
+        //1 de palabras
 
-    //longitus
-    const longitud = document.getElementById('longitud');
-    longitud.addEventListener('click', ()=>{
+        //longitus
+        const longitud = document.getElementById('longitud');
+        longitud.addEventListener('click', ()=>{
 
-        const palabra = document.getElementById('palabra').value;
-        const result = palabra.length;
-        alert(`La longitud de las palabra digitada ${palabra} es ${result}`);
+            const palabra = document.getElementById('palabra').value;
+            const result = palabra.length;
+            alert(`La longitud de las palabra digitada ${palabra} es ${result}`);
 
-    });
-  
-    //mayuscula
-    const mayuscula = document.getElementById('mayuscula');
+        });
+    
+        //mayuscula
+        const mayuscula = document.getElementById('mayuscula');
 
-    mayuscula.addEventListener('click', ()=>{
-        const palabra = document.getElementById('palabra').value;
-        const result = palabra.toUpperCase();
-        alert(`La palabra digitada ${palabra} a mayúscula es ${result}`);
-    });
-
-
-    //minuscula
-    const minuscula = document.getElementById('minuscula');
-    minuscula.addEventListener('click', ()=>{
-        const palabra = document.getElementById('palabra').value;
-        const result = palabra.toLowerCase();
-        alert(`La palabra digitada ${palabra} a minúscula es ${result}`);
-    });
-   
-
-    //crarcter 1
-    const caracter = document.getElementById('caracter');
-    caracter.addEventListener('click', ()=>{
-
-        const palabra = document.getElementById('palabra').value;
-        const result = palabra.charAt();
-
-        alert(`El primer caracter de la palabra digitada ${palabra} es ${result}`);
-    });
-
-  
-
-//2
-const temperature = document.getElementById('cal');
-temperature.addEventListener('click', ()=>{
-
-      const opcion = document.getElementById('opcion').value;
-      const usua = document.getElementById('tempe').value
-      parseInt(opcion);
-      parseFloat(usua);
-      
-      if (opcion==1) {
-       const grado=((5*(usua-32))/9);
-       alert(`los ${usua} grados Fahrenheit a Celsius son: ${grado}`);
-
-    } 
-    // fran a kel
-    else if (opcion==2) {
-        const grado=(((5*(usua-32))/9)+273.15);
-        alert(`los ${usua} grados Fahrenheit a Kelvin son: ${grado}`);
-
-    } 
-    //cel a faren
-    else if (opcion==3){
-        const grado=(((9*usua)/5)+32);
-        alert(`los ${usua} grados Celsius a Fahrenheit son: ${grado}`);
+        mayuscula.addEventListener('click', ()=>{
+            const palabra = document.getElementById('palabra').value;
+            const result = palabra.toUpperCase();
+            alert(`La palabra digitada ${palabra} a mayúscula es ${result}`);
+        });
 
 
-    } 
-    //cel a kel
-    else if (opcion==4){
-        const  grado=(usua+273.15);
-        alert(`los ${usua} grados Celsius a Kelvin son: ${grado}`);
+        //minuscula
+        const minuscula = document.getElementById('minuscula');
+        minuscula.addEventListener('click', ()=>{
+            const palabra = document.getElementById('palabra').value;
+            const result = palabra.toLowerCase();
+            alert(`La palabra digitada ${palabra} a minúscula es ${result}`);
+        });
+    
 
+        //crarcter 1
+        const caracter = document.getElementById('caracter');
+        caracter.addEventListener('click', ()=>{
+
+            const palabra = document.getElementById('palabra').value;
+            const result = palabra.charAt();
+
+            alert(`El primer caracter de la palabra digitada ${palabra} es ${result}`);
+        });
+
+    
+
+    //2
+    const temperature = document.getElementById('cal');
+    temperature.addEventListener('click', ()=>{
+
+        const opcion = document.getElementById('opcion').value;
+        const usua = document.getElementById('tempe').value
+        parseInt(opcion);
+        parseFloat(usua);
         
-    } 
-    //kel a faren
-    else if (opcion==5){
-        const grado=(((9*(usua-273.15))/5)+32);
-        alert(`los ${usua} grados Kelvin a Fahrenheit son: ${grado}`);
+        if (opcion==1) {
+        const grado=((5*(usua-32))/9);
+        alert(`los ${usua} grados Fahrenheit a Celsius son: ${grado}`);
 
-    } 
-    //kel a cel
-    else if (opcion==6){
-        const  grado=(usua-273.15);
-        alert(`los ${usua} grados Kelvin a Celsius son: ${grado}`);
+        } 
+        // fran a kel
+        else if (opcion==2) {
+            const grado=(((5*(usua-32))/9)+273.15);
+            alert(`los ${usua} grados Fahrenheit a Kelvin son: ${grado}`);
+
+        } 
+        //cel a faren
+        else if (opcion==3){
+            const grado=(((9*usua)/5)+32);
+            alert(`los ${usua} grados Celsius a Fahrenheit son: ${grado}`);
 
 
-    }
-})
+        } 
+        //cel a kel
+        else if (opcion==4){
+            const  grado=(usua+273.15);
+            alert(`los ${usua} grados Celsius a Kelvin son: ${grado}`);
+
+            
+        } 
+        //kel a faren
+        else if (opcion==5){
+            const grado=(((9*(usua-273.15))/5)+32);
+            alert(`los ${usua} grados Kelvin a Fahrenheit son: ${grado}`);
+
+        } 
+        //kel a cel
+        else if (opcion==6){
+            const  grado=(usua-273.15);
+            alert(`los ${usua} grados Kelvin a Celsius son: ${grado}`);
 
 
+        }
+    })
+
+    //3
+
+
+    
 
 
