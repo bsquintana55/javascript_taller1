308236728eb379afd58770bf8727f7db932260bf
bsquintana55 <bsquintana55@misena.edu.co>
2022-09-18 12:41:22 -0500

    formulario

--- assets/js/form.js
+++ assets/js/form.js
@@ -2,28 +2,24 @@ const formulario = document.getElementById('formulario');
 const inputs = document.querySelectorAll('#formulario input');
 
 const expresiones = {
-
  
     nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
     apellido: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
     correo: /^[a-zA-Z0-9_.+-]+@+[misena]+.[edu]+.[c]+[o]$/,
-   // fecha: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
+    //fecha: /^[0-9]+[0-9]+[0-9]+[0-9]+[0000-2002]$/,
     contraseña1:  /^.{4,12}$/, // 4 a 12 digitos.
-   // contraseña2: /^.{4,12}$/, // 4 a 12 digitos.
-    
+    contraseña2: /^.{4,12}$/, // 4 a 12 digitos.
 
 
 }
 
 const campos = {
-    
     nombre:false,
     apellido:false,
     correo:false,
-   // fecha:false,
-    //contraseña1:false,
-    //contraseña2:false
-
+	//fecha:false,
+    contraseña1:false,
+    contraseña2:false
 }
 
 const validarFormulario = (e) => {
@@ -38,18 +34,18 @@ const validarFormulario = (e) => {
         case "correo":
 			validarCampo(expresiones.correo, e.target, 'correo');
 		break;
-        
+       // case "fecha":
+		//	validarCampo(expresiones.fecha, e.target, 'fecha');
+		//break;
         case "contraseña1":
 			validarCampo(expresiones.contraseña1, e.target, 'contraseña1');
-			validarPassword2();
+			validarcontraseña2();
 		break;
-		case "password2":
-			validarPassword2();
+		case "contraseña2":
+			validarcontraseña2();
 		break;
 		
-		case "telefono":
-			validarCampo(expresiones.telefono, e.target, 'telefono');
-		break;
+	
 	}
 }
 
@@ -72,20 +68,50 @@ const validarCampo = (expresion, input, campo) => {
 }
 
 
+
+const validarcontraseña2 = () => {
+	const inputcontraseña1 = document.getElementById('contraseña1');
+	const inputcontraseña2 = document.getElementById('contraseña2');
+
+	if(inputcontraseña1.value !== inputcontraseña2.value){
+
+		document.getElementById(`grupo__contraseña2`).classList.add('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__contraseña2`).classList.remove('formulario__grupo-correcto');
+		document.querySelector(`#grupo__contraseña2 i`).classList.add('fa-times-circle');
+		document.querySelector(`#grupo__contraseña2 i`).classList.remove('fa-check-circle');
+		document.querySelector(`#grupo__contraseña2 .formulario__input-error`).classList.add('formulario__input-error-activo');
+		campos['contraseña1'] = false;
+
+	} else {
+
+		document.getElementById(`grupo__contraseña2`).classList.remove('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__contraseña2`).classList.add('formulario__grupo-correcto');
+		document.querySelector(`#grupo__contraseña2 i`).classList.remove('fa-times-circle');
+		document.querySelector(`#grupo__contraseña2 i`).classList.add('fa-check-circle');
+		document.querySelector(`#grupo__contraseña2 .formulario__input-error`).classList.remove('formulario__input-error-activo');
+		campos['contraseña1'] = true; 
+
+	}
+}
+
+
+
 inputs.forEach((input) => {
 	input.addEventListener('keyup', validarFormulario);
 	input.addEventListener('blur', validarFormulario);
-});
+}); 
+
 
 
 formulario.addEventListener('submit', (e) => {
 	e.preventDefault();
 
 	const terminos = document.getElementById('terminos');
-	if( campos.nombre && campos.apellido && campos.correo && campos.telefono && terminos.checked ){
+	if( campos.nombre && campos.apellido && campos.correo /* && campos.fecha*/ && campos.contraseña1 && terminos.checked ){
 		formulario.reset();
 
 		document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
+
 		setTimeout(() => {
 			document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
 		}, 5000);
@@ -93,7 +119,9 @@ formulario.addEventListener('submit', (e) => {
 		document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
 			icono.classList.remove('formulario__grupo-correcto');
 		});
-	} else {
+
+	} else { 
 		document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
 	}
+
 });
--- assets/js/taller.js
+++ assets/js/taller.js
@@ -11,7 +11,7 @@
 
             let palabra = document.getElementById('palabra').value;
             let result = palabra.length;
-            alert(`La longitud de las palabra digitada ${palabra} es ${result}`);
+            Swal.fire('La longitud de las palabra digitada '+palabra+' es '+result);
 
         });
     
@@ -21,7 +21,7 @@
         mayuscula.addEventListener('click', ()=>{
             let palabra = document.getElementById('palabra').value;
             let result = palabra.toUpperCase();
-            alert(`La palabra digitada ${palabra} a mayúscula es ${result}`);
+            Swal.fire(`La palabra digitada ${palabra} a mayúscula es ${result}`);
         });
 
 
@@ -30,7 +30,7 @@
         minuscula.addEventListener('click', ()=>{
             let palabra = document.getElementById('palabra').value;
             let result = palabra.toLowerCase();
-            alert(`La palabra digitada ${palabra} a minúscula es ${result}`);
+            Swal.fire(`La palabra digitada ${palabra} a minúscula es ${result}`);
         });
     
 
@@ -41,7 +41,7 @@
             let palabra = document.getElementById('palabra').value;
             let result = palabra.charAt();
 
-            alert(`El primer caracter de la palabra digitada ${palabra} es ${result}`);
+            Swal.fire(`El primer caracter de la palabra digitada ${palabra} es ${result}`);
         });
 
     
@@ -57,39 +57,39 @@
         
         if (opcion==1) {
         let grado=((5*(usua-32))/9);
-        alert(`los ${usua} grados Fahrenheit a Celsius son: ${grado}`);
+        Swal.fire(`los ${usua} grados Fahrenheit a Celsius son: ${grado}`);
 
         } 
         // fran a kel
         else if (opcion==2) {
             let grado=(((5*(usua-32))/9)+273.15);
-            alert(`los ${usua} grados Fahrenheit a Kelvin son: ${grado}`);
+            Swal.fire(`los ${usua} grados Fahrenheit a Kelvin son: ${grado}`);
 
         } 
         //cel a faren
         else if (opcion==3){
             let grado=(((9*usua)/5)+32);
-            alert(`los ${usua} grados Celsius a Fahrenheit son: ${grado}`);
+            Swal.fire(`los ${usua} grados Celsius a Fahrenheit son: ${grado}`);
 
 
         } 
         //cel a kel
         else if (opcion==4){
             let  grado=(usua+273.15);
-            alert(`los ${usua} grados Celsius a Kelvin son: ${grado}`);
+            Swal.fire(`los ${usua} grados Celsius a Kelvin son: ${grado}`);
 
             
         } 
         //kel a faren
         else if (opcion==5){
             let grado=(((9*(usua-273.15))/5)+32);
-            alert(`los ${usua} grados Kelvin a Fahrenheit son: ${grado}`);
+            Swal.fire(`los ${usua} grados Kelvin a Fahrenheit son: ${grado}`);
 
         } 
         //kel a cel
         else if (opcion==6){
             let  grado=(usua-273.15);
-            alert(`los ${usua} grados Kelvin a Celsius son: ${grado}`);
+            Swal.fire(`los ${usua} grados Kelvin a Celsius son: ${grado}`);
 
 
         }
@@ -109,14 +109,14 @@
            parseFloat(lado3)
 
            if (lado1==lado2 && lado1==lado3) {
-            alert(`Su triangulo es equilatero, ya que todos los lados son del mismo tamaño`)
+            Swal.fire(`Su triangulo es equilatero, ya que todos los lados son del mismo tamaño`)
            
            } else if( lado1==lado2 || lado2==lado3 || lado1==lado3) {   
-            alert(`Su triangulo es isósceles, ya que dos de los lados son del mismo tamaño`)
+            Swal.fire(`Su triangulo es isósceles, ya que dos de los lados son del mismo tamaño`)
            
            }
            else {
-            alert(`Su triangulo es escaleno, ya que ninguno de los lados son del mismo tamaño`)
+            Swal.fire(`Su triangulo es escaleno, ya que ninguno de los lados son del mismo tamaño`)
            
            }
         })
@@ -135,21 +135,21 @@
 
 
             if(nombre==1 && apellido==1 && materia==1){
-                alert("Has elegido bien, felicidades ")
+                Swal.fire("Has elegido bien, felicidades ")
             }else if(nombre==2 && apellido==2 && materia==2){
-                alert("Has elegido bien, felicidades ")
+                Swal.fire("Has elegido bien, felicidades ")
             }else if(nombre==3 && apellido==3 && materia==3){
-                alert("Has elegido bien, felicidades ")
+                Swal.fire("Has elegido bien, felicidades ")
             }else if(nombre==4 && apellido==4 && materia==4){
-                alert("Has elegido bien, felicidades ")
+                Swal.fire("Has elegido bien, felicidades ")
             }else if(nombre==5 && apellido==5 && materia==5){
-                alert("Has elegido bien, felicidades ")
+                Swal.fire("Has elegido bien, felicidades ")
             }else if(nombre==6 && apellido==6 && materia==6){
-                alert("Has elegido bien, felicidades ")
+                Swal.fire("Has elegido bien, felicidades ")
             }else if(nombre==7 && apellido==7 && materia==7 || materia==8){
-                  alert("Has elegido bien, felicidades ")
+                  Swal.fire("Has elegido bien, felicidades ")
             }else{
-                alert("mmh no esta correcto, prueba otra vez ")
+                Swal.fire("mmh no esta correcto, prueba otra vez ")
             }    
 })
 
@@ -166,7 +166,7 @@ AC.addEventListener('click',()=>{
       let LC = document.getElementById('LC').value;
       parseFloat(LC);
       let r = (LC*LC);
-      alert(`El Área del cuadrado es: ${r}`)
+      Swal.fire(`El Área del cuadrado es: ${r}`)
       
 })
 
@@ -176,7 +176,7 @@ PC.addEventListener('click',()=>{
       parseFloat(DC);
       let r = (DC*4);
 
-      alert(`El périmetro del cuadrado es: ${r}`)
+      Swal.fire(`El périmetro del cuadrado es: ${r}`)
       
 })
 
@@ -191,7 +191,7 @@ AT.addEventListener('click', ()=>{
 
    let r = (BT*UT)/2;
 
-      alert(`El area del triangulo es: ${r}`);
+   Swal.fire(`El area del triangulo es: ${r}`);
 });
 
 let PT = document.getElementById('PT');
@@ -203,7 +203,7 @@ PT.addEventListener('click', ()=>{
 
       let r =  parseFloat(L1)+parseFloat(L2)+parseFloat(L3);
 
-      alert(`El perimetro del triangulo es: ${r}`);  
+      Swal.fire(`El perimetro del triangulo es: ${r}`);  
 });
 
 
@@ -218,7 +218,7 @@ const pi= 3.14
       parseInt(RC)
       let r = pi* (parseFloat(RC)*2);
 
-      alert(`El area del circulo es: ${r.toFixed(2)}`);
+      Swal.fire(`El area del circulo es: ${r.toFixed(2)}`);
 
  });    
 
@@ -227,7 +227,7 @@ const pi= 3.14
       let I = document.getElementById('I').value;
 
       let r = (pi*2)*parseFloat(I);
-      alert(`El perimetro del circulo es: ${r.toFixed(2)}`);
+      Swal.fire(`El perimetro del circulo es: ${r.toFixed(2)}`);
  });
 
 
@@ -242,7 +242,7 @@ AR.addEventListener('click', ()=>{
       let AR = document.getElementById('AR').value;
       
       r= parseFloat(BR)*parseFloat(AR);
-      alert(`La base del rectangulo es: ${r}`);
+      Swal.fire(`La base del rectangulo es: ${r}`);
 });
 
 let PR = document.getElementById('PR');
@@ -251,7 +251,7 @@ PR.addEventListener('click', ()=>{
       let AP = document.getElementById('AP').value;
      
       r= (parseFloat(BP)+parseFloat(AP))*2;
-      alert(`El perimetro del rectangulo es: ${r}`);
+      Swal.fire(`El perimetro del rectangulo es: ${r}`);
 })
 
 
